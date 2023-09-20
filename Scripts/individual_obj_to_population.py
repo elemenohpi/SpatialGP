f66988168128b9ae38f00ac14072550d045a6fb9
Iliya <32390446+elemenohpi@users.noreply.github.com>
2023-09-20 18:59:47 -0400

    reg script

--- Scripts/individual_obj_to_population.py
+++ Scripts/individual_obj_to_population.py
@@ -1,7 +1,13 @@
 # This script takes a path to a directory of experiments, goes through every experiment folder
 # and if it finds individual pickled objects, it converts them to a population file and removes the individual files
+import _pickle
 import os
 import pickle
+import shutil
+
+from Population import BasePopulation
+from Individual import BaseIndividual
+from Programs import LGP
 
 
 def validate_path(path):
@@ -13,10 +19,10 @@ def validate_path(path):
 
 
 def remove_extra_dirs(path):
-    os.removedirs(os.path.join(path, "Error"))
-    os.removedirs(os.path.join(path, "Executable"))
-    os.removedirs(os.path.join(path, "Object"))
-    os.removedirs(os.path.join(path, "Subs"))
+    shutil.rmtree(os.path.join(path, "Error"))
+    shutil.rmtree(os.path.join(path, "Executable"))
+    shutil.rmtree(os.path.join(path, "Object"))
+    shutil.rmtree(os.path.join(path, "Subs"))
 
 
 def convert(path):
@@ -24,25 +30,48 @@ def convert(path):
     # instead of the population or not, then deletes the unnecessary data and converts the individuals to a single pop
     if not validate_path(path):
         return
-    # remove_extra_dirs(path)
+    remove_extra_dirs(path)
     population_path = os.path.join(path, "Population")
     population_dirs = os.listdir(population_path)
     for P in population_dirs:
         P_path = os.path.join(population_path, P)
         individuals = os.listdir(P_path)
+        individuals = sorted(individuals)
+        pop_obj = None
         for file in individuals:
             if not file.endswith(".sgp"):
                 continue
             file_path = os.path.join(P_path, file)
-            content = open(file_path, "rb")
-            individual = pickle.load(content)
-            pass
-            exit()
+            with open(file_path, "rb") as content:
+                failed = False
+                try:
+                    individual = pickle.load(content)
+                except (_pickle.UnpicklingError, EOFError):
+                    print(f"Failed to load: {file_path}")
+                    failed = True
+                if not pop_obj and not failed:
+                    pop_obj = BasePopulation.BasePopulation(individual.config, BaseIndividual.BaseIndividual, LGP.LGP)
+                    try:
+                        pop_obj.generation = individual.generation
+                    except AttributeError:
+                        pop_obj.generation = -1
+                    pop_obj.pop = []
+                    pop_obj.pop.append(individual)
+                elif pop_obj:
+                    pop_obj.pop.append(individual)
+        with open(os.path.join(P_path, "pop.sgp"), 'wb') as object_file:
+            pickle.dump(pop_obj, object_file)
+            print(f"{P} done")
+        for file in individuals:
+            if not file.endswith(".sgp"):
+                continue
+            if file == "pop.sgp":
+                continue
+            os.remove(os.path.join(P_path, file))
 
 
-
-experiments = "../HPCC_Experiments/"
-key = "Topology_circle_mutation_med_high_single"
+experiments = "../../Results/F10"
+key = ""
 
 if __name__ == "__main__":
     directories = os.listdir(experiments)
