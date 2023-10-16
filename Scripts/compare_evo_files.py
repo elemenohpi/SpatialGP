af7cfe42490bb708d4638617d4705f79661e1f2d
Iliya <32390446+elemenohpi@users.noreply.github.com>
2023-10-16 16:58:49 -0400

    Update compare_evo_files.py

--- Scripts/compare_evo_files.py
+++ Scripts/compare_evo_files.py
@@ -49,6 +49,6 @@ def compare_experiments(path, gen):
 
 
 if __name__ == "__main__":
-    compare_experiments("../../Results/Compressed/all_conf", "min")
+    compare_experiments("../loop/", "min")
 
 
